cec23eb3324199b391f37f385e521f6d81e75fa7
robertocarrazzoni <roberto.carrazzoni17@gmail.com>
2022-07-28 13:50:06 -0300

    Criação e exclusão de pensamentos finalizado

--- 11_toughts/controllers/AuthController.js
+++ 11_toughts/controllers/AuthController.js
@@ -7,6 +7,39 @@ module.exports = class AuthController {
         res.render('auth/login')
     }
 
+    static async loginPost(req, res) {
+        const { email, password } = req.body
+
+        // find user
+        const user = await User.findOne({where: {email: email}})
+
+        if (!user) {
+            req.flash('message', 'Usuário não encontrado!')
+            res.render('auth/login')
+
+            return
+        }
+        //check if password match
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if (!passwordMatch) {
+            req.flash('message', 'Senha inválida!')
+            res.render('auth/login')
+
+            return
+        }
+
+        //initialize session
+
+        req.session.userid = user.id
+
+        req.flash('message', 'Autenticação realizada com sucesso!')
+
+        req.session.save(() => {
+        res.redirect('/')
+        })
+    }
+
     static register(req, res) {
         res.render('auth/register')
     }
@@ -21,5 +54,45 @@ module.exports = class AuthController {
 
             return
         }
+
+        //check if user exists
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+
+        if (checkIfUserExists) {
+            req.flash('message', 'O email já está em uso!')
+            res.render('auth/register')
+        }
+
+        //Create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+           const createdUser = await User.create(user)
+            
+            
+            //initialize session
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso!')
+            
+            req.session.save(() => {
+                res.redirect('/')
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/login')
     }
 }
