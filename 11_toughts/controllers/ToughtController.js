7a9ada6f82d42bc5bab5d7db8e141e6a0a71d639
robertocarrazzoni <roberto.carrazzoni17@gmail.com>
2022-07-29 11:39:17 -0300

    finalizando o projeto toughts

--- 11_toughts/controllers/ToughtController.js
+++ 11_toughts/controllers/ToughtController.js
@@ -1,9 +1,42 @@
 const Tought = require('../models/Tought')
 const User = require('../models/User')
 
+const {Op} = require('sequelize')
+
 module.exports = class ToughtController {
     static async showToughts(req, res) {
-        res.render('toughts/home')
+
+        let search = ''
+
+        if (req.query.search) {
+            search = req.query.search
+        }
+
+        let order = 'DESC'
+
+        if (req.query.order === 'old') {
+            order = 'ASC'
+        } else {
+            order = 'DESC'
+        }
+        
+        const toughtsData = await Tought.findAll({
+            include: User,
+            where: {
+                title: {[Op.like]: `%${search}%`},
+            },
+            order: [['createdAt', order]]
+        })
+
+        const toughts = toughtsData.map((result) => result.get({plain: true}))
+
+        let toughtsQty = toughts.length
+
+        if (toughtsQty === 0) {
+            toughtsQty = false
+        }
+       
+        res.render('toughts/home', { toughts, search, toughtsQty })
     }
 
     static async dashboard(req, res) {
@@ -72,4 +105,38 @@ module.exports = class ToughtController {
             console.log('Aconteceu um erro:' + error)
         }
     }
+
+    static async updateTought(req, res) {
+        
+        const id = req.params.id
+
+        const tought = await Tought.findOne({ where: {id: id }, raw: true })
+
+        res.render('toughts/edit', { tought })
+    }
+
+    static async updateToughtSave(req, res) {
+
+        const id = req.body.id
+        
+        const tought = {
+            title: req.body.title,
+        }
+
+        try {
+
+            await Tought.update(tought, { where: {id: id} })
+            req.flash('message', 'Pensamento atualizado com sucesso!')
+
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+
+        })
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
 }
