492434297b15d628702ab024472c454b000ff3d3
robertocarrazzoni <roberto.carrazzoni17@gmail.com>
2022-08-02 15:19:04 -0300

    mongodb

---
+++ 12_mongodb/3_inserindo_dados/db/conn.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb')
+
+const uri = "mongodb://localhost:27017/testemongodb"
+
+const client = new MongoClient(uri)
+
+async function run() {
+    try {
+
+        await client.connect()
+        console.log('conectando ao mongodb')
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+run()
+
+module.exports = client
